214ae838070e7c6e44675c6ef5994252813ba9c9
tuck <104166631@qq.com>
2020-06-13 17:25:30 +0800

    库名称修改为TCJSONModel

---
+++ TCModel/Classes/TCModel.h
@@ -0,0 +1,16 @@
+//
+//  TCModel.h
+//  Pods
+//
+//  Created by fengunion on 2020/6/13.
+//
+
+#ifndef TCModel_h
+#define TCModel_h
+
+#import "NSObject+TCModel.h"
+#import "TCJSONKeyMapper.h"
+#import "TCJSONValueTransformer.h"
+#import "TCModelError.h"
+
+#endif /* TCModel_h */
